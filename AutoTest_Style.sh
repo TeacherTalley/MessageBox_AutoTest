b27ca540478564112fc2bfc908e6275bcf4e1c18
Terry Talley <ttalley@cub.uca.edu>
2024-01-16 13:58:56 -0600

    Set explicit style filters for GitHub Classroom

--- AutoTest_Style.sh
+++ AutoTest_Style.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 pip install cpplint
 cd MessageBox_AutoTest
-cpplint main.cpp MessageBox.h
+
+srcfiles="../main.cpp ../MessageBox.h"
+echo "Source files:" $srcfiles
+
+# for some reason, GitHub Classroom environment does not use cpplint.cfg
+# explcitly ignore some style checks
+filters=-legal/copyright,-build/header_guard,-whitespace/braces,-runtime/explicit,-whitespace/newline,-whitespace/end_of_line
+
+cpplint --filter=$filters $srcfiles
