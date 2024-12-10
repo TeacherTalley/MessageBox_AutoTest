f807d8022135300ff6726168deb64f9e8f2bf54f
Terry Talley <ttalley@cub.uca.edu>
2024-12-10 15:11:09 -0600

    Initial commit of Spring 2025 version of autotesting.

--- AutoTest_all.sh
+++ AutoTest_all.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
+#--------------------------------------------------------------------------
+# File: AutoTest_all.sh
+# Programmer: Michelle Talley
+# Copyright 2024 Michelle Talley University of Central Arkansas
+#--------------------------------------------------------------------------
+repo=MessageBox_AutoTest
+echo "#################### START: AutoTest Setup ##################################"
+echo " To be consistent with the grading environment, assume we are starting out "
+echo " in the source directory (i.e., the parent of the AutoTest directory)."
+echo " You will get a cd error if you execute directly from the AutoTest directory."
+echo "#############################################################################"
+cd $repo
+echo
 echo
 echo "#################### START: AutoTest Results #####################"
-srcfiles="main.cpp MessageBox.h"
-echo "Source files:" $srcfiles
-echo
-echo "--- Checking code format (cpplint) ---"
-#cpplint --filter=-legal/copyright,-whitespace/braces,-runtime/explicit,-whitespace/newline,-whitespace/end_of_line *.cpp *.h
-cpplint $srcfiles
+./AutoTest_Style.sh $repo main.cpp LinkedList.h
 echo
 cd build
 echo
-echo "--- Checking main output (diff) ---"
-./main > test_main_output.txt
-diff ../AutoTest_main_output.txt test_main_output.txt
+../AutoTest_OutputTest.sh $repo/build
 echo
 echo "--- Unit testing (googletest - all tests at once) ---"
 ctest
 echo
+# Note: GitHub Classroom auto-grading detects non-zero exit codes as errors and reports.
+# As a result, AutoTest_gtests can be run directly in the auto-grading environment.
+# The AutoTest_single_gtest script runs the individual tests a nicer error message for
+# use when testing locally (i.e., in a shell).
+#
+# GitHub Classroom auto-grading runs the following commands from the current
+# directory of the project being tested.  To similate that here, we need to
+# change to the project directory before running the tests.
+#
+cd ../..
 echo "--- Unit testing (single test at a time) ---"
-./AutoTest_gtests --gtest_filter=MessageBox.SendAndReceive
-./AutoTest_gtests --gtest_filter=MessageBox.OutOfRange
-./AutoTest_gtests --gtest_filter=MessageBox.FullAndEmpty
-./AutoTest_gtests --gtest_filter=MessageBox.CountAndSize
-./AutoTest_gtests --gtest_filter=MessageBox.Print
+./$repo/AutoTest_gtest.sh MessageBoxTest.SendAndReceive
+./$repo/AutoTest_gtest.sh MessageBoxTest.OutOfRange
+./$repo/AutoTest_gtest.sh MessageBoxTest.FullAndEmpty
+./$repo/AutoTest_gtest.sh MessageBoxTest.CountAndSize
+./$repo/AutoTest_gtest.sh MessageBoxTest.Print
 echo
-cd ..
 echo "#################### END: AutoTest Results   #####################"
 echo
